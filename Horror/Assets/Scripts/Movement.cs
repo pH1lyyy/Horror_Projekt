58cfad686bd74c5e5bc2a403d32881debe58b81b
pH1lyyy <karamustafazsl9@gmail.com>
2025-05-11 11:54:43 +0200

    Jump, Sprint and Crouching

--- Horror/Assets/Scripts/Movement.cs
+++ Horror/Assets/Scripts/Movement.cs
@@ -3,21 +3,62 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Transform player;
-    public float speed;
+    public float speed = 5f;
+    public float sprintSpeed = 4f;
+    public float crouchSpeed = 2.5f;
+    public float jumpForce = 1f;
     public float x;
     public float z;
 
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isCrouching = false;
+
     void Start()
     {
-        
+        controller = player.GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float currentSpeed = speed;
+
+        // Sprint
+        if (Input.GetKey(KeyCode.LeftShift) && !isCrouching)
+        {
+            currentSpeed = sprintSpeed;
+        }
+
+        // Crouch
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = !isCrouching;
+            controller.height = isCrouching ? 1f : 2f;
+        }
+
+        if (isCrouching)
+        {
+            currentSpeed = crouchSpeed;
+        }
+
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
 
-        player.Translate(x * speed * Time.deltaTime, 0, z * speed * Time.deltaTime);
+        Vector3 move = player.right * x + player.forward * z;
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
+        // Jump
+        if (controller.isGrounded)
+        {
+            velocity.y = -2f;
+
+            if (Input.GetKeyDown(KeyCode.Space) && !isCrouching)
+            {
+                velocity.y = Mathf.Sqrt(jumpForce * -2f * Physics.gravity.y);
+            }
+        }
+
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
