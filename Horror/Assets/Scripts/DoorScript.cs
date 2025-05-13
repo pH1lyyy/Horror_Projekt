c82c04d2fde9cc3d8e5ef013d31973c86cb24bd3
pH1lyyy <karamustafazsl9@gmail.com>
2025-05-13 19:15:07 +0200

    Opendoors animation and system(still not working)

---
+++ Horror/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class DoorScript : MonoBehaviour
+{
+    Animator doorAnimation;
+    public Transform player;
+    public float detectionDistance = 3f;
+    public LayerMask playerLayer;
+    private bool isPlayerNear = false;
+    public string keyLayerName = "";
+
+
+    void Start()
+    {
+        doorAnimation = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        CheckPlayerDistance();
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.F))
+        {
+            OnDoorButtonPress();
+        }
+    }
+    void CheckPlayerDistance()
+    {
+        RaycastHit hit;
+        Vector3 directiontoPlayer = player.position - transform.position;
+
+        if (Physics.Raycast(transform.position, directiontoPlayer, out hit, detectionDistance, playerLayer))
+        {
+            if (hit.transform == player)
+            {
+                isPlayerNear = true;
+                return;
+            }
+        }
+        isPlayerNear = false;
+
+    }
+    public void OnDoorButtonPress()
+    {
+        OpenDoor();
+    }
+    void OpenDoor()
+    {
+        if (doorAnimation != null)
+        {
+            if (!string.IsNullOrEmpty(keyLayerName))
+            {
+                bool playerHasKey = false;
+                foreach (Transform key in player)
+                {
+                    if (key.gameObject.layer == LayerMask.NameToLayer(keyLayerName))
+                    {
+                        playerHasKey = true;
+                        break;
+                    }
+
+                }
+                if(!playerHasKey)
+                {
+                    Debug.Log("You need a key to open this door.");
+                    return;
+                }
+
+            }
+            doorAnimation.SetTrigger("OpenDoor");
+        }
+    }
+}
