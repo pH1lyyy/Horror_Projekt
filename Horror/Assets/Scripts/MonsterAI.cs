0184ada02a1a1da39ad8ec7973d19fd168571c78
pH1lyyy <karamustafazsl9@gmail.com>
2025-05-20 18:00:08 +0200

    Updated Monster(need to add animations)

--- Horror/Assets/Scripts/MonsterAI.cs
+++ Horror/Assets/Scripts/MonsterAI.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
-using UnityEngine.AI;
 
 public class MonsterAI : MonoBehaviour
 {
-    NavMeshAgent navMeshAgent;
+    [Header("Sound detection")]
+
+    UnityEngine.AI.NavMeshAgent navMeshAgent;
+
     public float moveSpeed = 3.5f;
     public Transform startPosition;
     private Vector3 soundLocation;
@@ -14,37 +16,64 @@ public class MonsterAI : MonoBehaviour
     private bool isAttacking = false;
     private bool isWaiting = false;
     private bool isDead = false;
+    bool SoundHeard = false;
 
-    private void Start()
+    [Header("Monester states")]
+    public Transform player;
+    public float detectionRadius = 7f;
+    public float attackRange = 2f;
+    public float attackCooldown = 1f;
+    float lastAttackTime = 0f;
+
+    private Animator animator;
+    void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         navMeshAgent.speed = moveSpeed;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        if (isDead) return;
+
         if (isReturning)
         {
             ReturnToStart();
         }
+        else if (isAttacking)
+        {
+            AttackPlayer();
+        }
+        else if (isChasing && SoundHeard)
+        {
+            ChasePlayer();
+        }
+        else if (!isWaiting)
+        {
+            LookingForPlayer();
+        }
+        UpdateAnimations();
     }
-  
+
     public void OnSoundHeard(Vector3 location)
     {
-        if(isDead) return;
+        if (isDead) return;
+
         soundLocation = location;
+        SoundHeard = true;
         isReturning = false;
         isChasing = false;
         isAttacking = false;
         isWaiting = false;
         MoveToSoundLocation();
-    } 
+    }
     void MoveToSoundLocation()
     {
         navMeshAgent.SetDestination(soundLocation);
         if (Vector3.Distance(transform.position, soundLocation) <= navMeshAgent.stoppingDistance)
         {
-          StartCoroutine(WaitBeforeReturning());
+            StartCoroutine(WaitBeforeReturning());
         }
     }
     private IEnumerator WaitBeforeReturning()
@@ -53,6 +82,7 @@ public class MonsterAI : MonoBehaviour
         yield return new WaitForSeconds(10f);
         isWaiting = false;
         isReturning = true;
+        SoundHeard = false;
     }
 
     void ReturnToStart()
@@ -61,6 +91,70 @@ public class MonsterAI : MonoBehaviour
         if (Vector3.Distance(transform.position, startPosition.position) <= navMeshAgent.stoppingDistance)
         {
             isReturning = false;
+        }
+    }
+    void LookingForPlayer()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRadius);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.CompareTag("Player"))
+            {
+                isChasing = true;
+                isReturning = false;
+                break;
+            }
+        }
+    }
+    void ChasePlayer()
+    {
+        navMeshAgent.SetDestination(player.position);
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= attackRange)
+        {
+            navMeshAgent.isStopped = true;
+            isChasing = false;
+            isAttacking = true;
+        }
+        else if (distanceToPlayer > detectionRadius)
+        {
+            isChasing = false;
+            isReturning = true;
+        }
+    }
+    void AttackPlayer()
+    {
+        if (Time.time > lastAttackTime + attackCooldown)
+        {
+            CharacterController playerController = player.GetComponent<CharacterController>();
+            if (playerController != null)
+            {
+                Debug.Log("Attacking player!");
+            }
+            lastAttackTime = Time.time;
+
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+            if (distanceToPlayer > attackRange)
+            {
+                navMeshAgent.isStopped = false;
+                isChasing = true;
+                isAttacking = false;
+            }
+        }
+    }
+    void UpdateAnimations()
+    {
+        bool isMoving = navMeshAgent.velocity.magnitude > 0.1f;
+        animator.SetBool("isWalking", isMoving);
+        animator.SetBool("isAttacking", isAttacking);
+        if (!isMoving && !isAttacking)
+        {
+            animator.SetBool("isIdle", true);
+        }
+        else
+        {
+            animator.SetBool("isIdle", false);
         }
     }
 }
